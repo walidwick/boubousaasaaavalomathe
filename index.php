02876a73f629d38cbc1fc60c7bd35aa32e8fc44c
walidwick <66850558+walidwick@users.noreply.github.com>
2021-04-05 19:09:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
 
-echo 'Hello, this is a Valorant API for Twitchbot (Nightbot, etc..) developed by made by <a href="https://github.com/Rehkloos" target="_self">Andre Saddler</a> using <a href="TRN" target="_self">Valorant TRN</a> API '."<br>";
-echo 'Website: <a href="https://rehkloos.com" target="_self">CLICK HERE</a> '."<br><br>";
-
-echo 'How-To: $(urlfetch https://valorant-nightbot.herokuapp.com/valorant-stats.php?nick=(Name)&tag=(ID number after #)&command=(stats or rank)';
+echo 'Valorant API for Nightbot. Made By <a href="https://www.twitch.tv/walid_zahri" target="_self">Walid Zahri</a> '."<br>";
+echo 'Channel : <a href="https://www.twitch.tv/mathematicien" target="_self">Mathematicien</a> ';
