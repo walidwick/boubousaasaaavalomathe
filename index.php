c94d539d1fa15ba7548a48c983fc1a83a3c9b77c
walidwick <66850558+walidwick@users.noreply.github.com>
2021-06-11 23:43:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 
 echo 'Valorant API for Nightbot. Made By <a href="https://www.twitch.tv/walid_zahri" target="_self">Walid Zahri</a> '."<br>";
-echo 'Channel : <a href="https://www.twitch.tv/mathematicien" target="_self">Mathematicien</a> ';
+echo 'Channels using this API : <a href="https://www.twitch.tv/mathematicien" target="_self">Mathematicien</a> - <a href="https://www.twitch.tv/bazouya" target="_self">Bazouya</a> - <a href="https://www.twitch.tv/rizkou" target="_self">Rizkou</a>';
