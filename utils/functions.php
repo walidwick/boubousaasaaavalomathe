8ddf1dac6b8f161a8d1e209c4759da1cbcb44d87
walidwick <66850558+walidwick@users.noreply.github.com>
2021-06-26 01:19:37 +0100

    Update functions.php

--- utils/functions.php
+++ utils/functions.php
@@ -3,8 +3,7 @@ function _getJSON($url)
 {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // so curl_exec returns data
-    // grab URL and pass it to the browser; store returned data
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
     $curlRes = curl_exec($ch);
 
     if (curl_errno($ch))
@@ -12,9 +11,7 @@ function _getJSON($url)
         echo 'Error:' . curl_error($ch);
     }
 
-    // close cURL resource, and free up system resources
     curl_close($ch);
-    // Decode returned JSON so it can be handled as a multi-dimensional associative array
     return json_decode($curlRes, true);
 };
 
@@ -25,7 +22,6 @@ $h = floor(($ss%86400)/3600);
 $d = floor(($ss%2592000)/86400);
 $M = floor($ss/2592000);
 
-//return "$M months, $d days, $h hours, $m minutes, $s seconds";
 return "$d d $h h $m m $s s";
 }
 
