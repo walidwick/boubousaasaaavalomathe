49647a6b7344e9b1bcf4ada8abf8d6bf16e8e70e
walidwick <66850558+walidwick@users.noreply.github.com>
2021-06-26 01:16:55 +0100

    Update valorant-stats.php

--- valorant-stats.php
+++ valorant-stats.php
@@ -15,22 +15,6 @@ $riotid = $player . '%23' . $tag;
 
 switch ($request)
 {
-    case "stats":
-        $base = _getJSON('https://api.tracker.gg/api/v2/valorant/standard/profile/riot/' . $player . '%23' . $tag);
-
-        // Valortant stat calls
-        $kills = $base['data']['segments'][0]['stats']['kills']['value'];
-        $mKills = intval($base['data']['segments'][0]['stats']['mostKillsInAGame']['value']);
-        $deaths = intval($base['data']['segments'][0]['stats']['deaths']['value']);
-        $kdr = intval($base['data']['segments'][0]['stats']['kDRatio']['value']);
-        $wins = intval($base['data']['segments'][0]['stats']['wins']['value']);
-        $winr  = intval($base['data']['segments'][0]['stats']['wlratio']['value']);
-        //timePlayedTotal
-        $timeplayed  = $base['data']['segments'][0]['stats']['timePlayedTotal']['value'];
-
-
-        echo "Time Played: " . timeSeconds($timeplayed) . " | Wins: " . $wins . " | Win/Loss: " . $winr . "% | Kills: " . $kills . " | Most Kills in Game: " . $mKills . " | KDR: " . round($kdr) . " | Deaths: " . $deaths ." (" . urldecode($riotid) . ")";
-    break;
     // tal gheda
     case "rank":
         //        $base = _getJSON('https://api.tracker.gg/api/v2/valorant/standard/profile/riot/mathematicien%231687');
@@ -56,7 +40,7 @@ switch ($request)
         $tier = $base['data']['ranking_in_tier'];
 
         $rank = $base['data']['currenttierpatched'];
-    echo "Rank : " . $rank . " | Tier : " . $tier . " | Elo : " . $elo ."";
+    echo "" . $rank . " | Tier : " . $tier . " | Elo : " . $elo ."";
     break;
     case "rankrizkou":
         $base = _getJSON('https://api.henrikdev.xyz/valorant/v1/mmr/eu/rizk/god');
@@ -81,7 +65,7 @@ switch ($request)
         $tier = $base['data']['ranking_in_tier'];
 
         $rank = $base['data']['currenttierpatched'];
-    echo "Rank : " . $rank . " | Tier : " . $tier . " | Elo : " . $elo ."";
+    echo "" . $rank . " | Tier : " . $tier . " | Elo : " . $elo ."";
     break;
     case "rankbazouzou":
         $base = _getJSON('https://api.henrikdev.xyz/valorant/v1/mmr/eu/bazouzou/baz');
@@ -90,15 +74,15 @@ switch ($request)
         $tier = $base['data']['ranking_in_tier'];
 
         $rank = $base['data']['currenttierpatched'];
-    echo "Rank : " . $rank . " | Tier : " . $tier . " | Elo : " . $elo ."";
+    echo "" . $rank . " | Tier : " . $tier . " | Elo : " . $elo ."";
     break;
     case "lastgame":
         $base = _getJSON('https://api.henrikdev.xyz/valorant/v1/mmr/eu/mathematicien/1687');
 
         $lg = $base['data']['mmr_change_to_last_game'];
         
-        
-        echo "Ha ch7al zadoh ola ne9soh f lastgame : " . $lg . "" ;
+        //echo "Ha ch7al zadoh ola ne9soh f lastgame : " . $lg . "" ;
+        echo "ask chi wa7d f chat a khay ma39elch achno dar Kappa" ;
     break;
         case "mostkills":
         $base = _getJSON('https://api.tracker.gg/api/v2/valorant/standard/profile/riot/mathematicien%231687');
@@ -135,25 +119,6 @@ switch ($request)
         
         echo "Mathe jab f comptetive : " . $clutches . " clutches" ;
     break;
-    case "time":
-        $base = _getJSON('https://api.tracker.gg/api/v2/valorant/standard/profile/riot/' . $player . '%23' . $tag);
-
-        // Valortant stat calls
-        $timeplayedCOMP  = $base['data']['segments'][0]['stats']['timePlayed']['value']; // competitive
-        $timeplayedDMS   = $base['data']['segments'][1]['stats']['timePlayed']['value']; // deathmatch
-        $timeplayedSPKR  = $base['data']['segments'][2]['stats']['timePlayed']['value']; // spikerush
-        $timeplayedURTD  = $base['data']['segments'][3]['stats']['timePlayed']['value']; // unrated
-        $TTP = $timeplayedCOMP + $timeplayedDMS + $timeplayedSPKR + $timeplayedURTD; // Total Sum of time played between all playlists
-
-        //TimeMilliseconds to Days conversion
-        $time = $TTP / 1000;
-        $days = floor($time / (24*60*60));
-        $hours = floor(($time - ($days*24*60*60)) / (60*60));
-        $minutes = floor(($time - ($days*24*60*60)-($hours*60*60)) / 60);
-        $seconds = ($time - ($days*24*60*60) - ($hours*60*60) - ($minutes*60)) % 60;
-
-        echo "Total Time Played: " . $days.'d '.$hours.'h '.$minutes.'m ' .$seconds.'s '. " (" . urldecode($riotid) . ")";
-    break;
     default:
-        echo "ask walid mnach l mochkil";
+        echo "fin ghadi a dak rass l 9alwa xD";
 }
