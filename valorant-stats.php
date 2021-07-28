b5c882af7299904a42690f24ed1d8ecf59192eb7
walidwick <66850558+walidwick@users.noreply.github.com>
2021-07-28 20:38:17 +0100

    Update valorant-stats.php

--- valorant-stats.php
+++ valorant-stats.php
@@ -59,22 +59,6 @@ switch ($request)
         
     echo "Main account Rank : " . $rank . " / Tier : " . $tier . "";
     break;
-    case "rankrico":
-        $base = _getJSON('https://api.henrikdev.xyz/valorant/v1/mmr/eu/Superico/8263');
-        
-        $tier = $base['data']['ranking_in_tier'];
-        $rank = $base['data']['currenttierpatched'];
-        
-    echo "" . $rank . " tier : " . $tier . "";
-    break;
-    case "rankera":
-        $base = _getJSON('https://api.henrikdev.xyz/valorant/v1/mmr/eu/eraroxeur/euw');
-        
-        $tier = $base['data']['ranking_in_tier'];
-        $rank = $base['data']['currenttierpatched'];
-        
-    echo "" . $rank . " | " . $tier . "/100";
-    break;
     case "ranksmurf":
         $base = _getJSON('https://api.henrikdev.xyz/valorant/v1/mmr/eu/Ninja1di1Milano/euw');
         
