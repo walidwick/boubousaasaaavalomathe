d28fc2f11a68ad8d84485a0eed9eac7edc0a3ef8
walidwick <66850558+walidwick@users.noreply.github.com>
2021-12-26 01:30:13 +0100

    Update valorant-stats.php

--- valorant-stats.php
+++ valorant-stats.php
@@ -25,7 +25,7 @@ switch ($request)
         $rank = $base['data']['currenttierpatched'];
         $top = $base2['data']['segments'][0]['stats']['rank']['rank'];
 
-        echo "" . $rank ."" . $tier ."RR | #" . $top ." EU ";
+        echo "" . $rank ." " . $tier ."RR | #" . $top ." EU ";
     break;
     case "minirank":
         $base = _getJSON('https://api.henrikdev.xyz/valorant/v1/mmr/eu/Mathematicien/1687');
