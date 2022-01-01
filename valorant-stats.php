f14c3c5075106f829c7fd7eab1e935ea0a98f38f
walidwick <66850558+walidwick@users.noreply.github.com>
2022-01-02 00:52:40 +0100

    Update valorant-stats.php

--- valorant-stats.php
+++ valorant-stats.php
@@ -410,48 +410,48 @@ switch ($request)
         //lastmatch test
     case "teammates":
         $base = _getJSON('https://api.henrikdev.xyz/valorant/v3/by-puuid/matches/eu/fae583c4-c8d4-54ee-ae01-2b0c084865df');
-        $gamemode = $base['data'][1]['metadata']['mode'];
-        $server = $base['data'][1]['metadata']['cluster'];
+        $gamemode = $base['data'][0]['metadata']['mode'];
+        $server = $base['data'][0]['metadata']['cluster'];
         
-        $player1name = $base['data'][1]['players']['red'][0]['name'];
-        $player1tag = $base['data'][1]['players']['red'][0]['tag'];
-        $player1character = $base['data'][1]['players']['red'][0]['character'];
-        $player1rank = $base['data'][1]['players']['red'][0]['currenttier_patched'];
-        $player1kills = $base['data'][1]['players']['red'][0]['stats']['kills'];
-        $player1deaths = $base['data'][1]['players']['red'][0]['stats']['deaths'];
-        $player1assists = $base['data'][1]['players']['red'][0]['stats']['assists'];
+        $player1name = $base['data'][0]['players']['red'][0]['name'];
+        $player1tag = $base['data'][0]['players']['red'][0]['tag'];
+        $player1character = $base['data'][0]['players']['red'][0]['character'];
+        $player1rank = $base['data'][0]['players']['red'][0]['currenttier_patched'];
+        $player1kills = $base['data'][0]['players']['red'][0]['stats']['kills'];
+        $player1deaths = $base['data'][0]['players']['red'][0]['stats']['deaths'];
+        $player1assists = $base['data'][0]['players']['red'][0]['stats']['assists'];
         
-        $player2name = $base['data'][1]['players']['red'][1]['name'];
-        $player2tag = $base['data'][1]['players']['red'][1]['tag'];
-        $player2character = $base['data'][1]['players']['red'][1]['character'];
-        $player2rank = $base['data'][1]['players']['red'][1]['currenttier_patched'];
-        $player2kills = $base['data'][1]['players']['red'][1]['stats']['kills'];
-        $player2deaths = $base['data'][1]['players']['red'][1]['stats']['deaths'];
-        $player2assists = $base['data'][1]['players']['red'][1]['stats']['assists'];
+        $player2name = $base['data'][0]['players']['red'][1]['name'];
+        $player2tag = $base['data'][0]['players']['red'][1]['tag'];
+        $player2character = $base['data'][0]['players']['red'][1]['character'];
+        $player2rank = $base['data'][0]['players']['red'][1]['currenttier_patched'];
+        $player2kills = $base['data'][0]['players']['red'][1]['stats']['kills'];
+        $player2deaths = $base['data'][0]['players']['red'][1]['stats']['deaths'];
+        $player2assists = $base['data'][0]['players']['red'][1]['stats']['assists'];
         
-        $player3name = $base['data'][1]['players']['red'][2]['name'];
-        $player3tag = $base['data'][1]['players']['red'][2]['tag'];
-        $player3character = $base['data'][1]['players']['red'][2]['character'];
-        $player3rank = $base['data'][1]['players']['red'][2]['currenttier_patched'];
-        $player3kills = $base['data'][1]['players']['red'][2]['stats']['kills'];
-        $player3deaths = $base['data'][1]['players']['red'][2]['stats']['deaths'];
-        $player3assists = $base['data'][1]['players']['red'][2]['stats']['assists'];
+        $player3name = $base['data'][0]['players']['red'][2]['name'];
+        $player3tag = $base['data'][0]['players']['red'][2]['tag'];
+        $player3character = $base['data'][0]['players']['red'][2]['character'];
+        $player3rank = $base['data'][0]['players']['red'][2]['currenttier_patched'];
+        $player3kills = $base['data'][0]['players']['red'][2]['stats']['kills'];
+        $player3deaths = $base['data'][0]['players']['red'][2]['stats']['deaths'];
+        $player3assists = $base['data'][0]['players']['red'][2]['stats']['assists'];
         
-        $player4name = $base['data'][1]['players']['red'][3]['name'];
-        $player4tag = $base['data'][1]['players']['red'][3]['tag'];
-        $player4character = $base['data'][1]['players']['red'][3]['character'];
-        $player4rank = $base['data'][1]['players']['red'][3]['currenttier_patched'];
-        $player4kills = $base['data'][1]['players']['red'][3]['stats']['kills'];
-        $player4deaths = $base['data'][1]['players']['red'][3]['stats']['deaths'];
-        $player4assists = $base['data'][1]['players']['red'][3]['stats']['assists'];
+        $player4name = $base['data'][0]['players']['red'][3]['name'];
+        $player4tag = $base['data'][0]['players']['red'][3]['tag'];
+        $player4character = $base['data'][0]['players']['red'][3]['character'];
+        $player4rank = $base['data'][0]['players']['red'][3]['currenttier_patched'];
+        $player4kills = $base['data'][0]['players']['red'][3]['stats']['kills'];
+        $player4deaths = $base['data'][0]['players']['red'][3]['stats']['deaths'];
+        $player4assists = $base['data'][0]['players']['red'][3]['stats']['assists'];
         
-        $player5name = $base['data'][1]['players']['red'][4]['name'];
-        $player5tag = $base['data'][1]['players']['red'][4]['tag'];
-        $player5character = $base['data'][1]['players']['red'][4]['character'];
-        $player5rank = $base['data'][1]['players']['red'][4]['currenttier_patched'];
-        $player5kills = $base['data'][1]['players']['red'][4]['stats']['kills'];
-        $player5deaths = $base['data'][1]['players']['red'][4]['stats']['deaths'];
-        $player5assists = $base['data'][1]['players']['red'][4]['stats']['assists'];
+        $player5name = $base['data'][0]['players']['red'][4]['name'];
+        $player5tag = $base['data'][0]['players']['red'][4]['tag'];
+        $player5character = $base['data'][0]['players']['red'][4]['character'];
+        $player5rank = $base['data'][0]['players']['red'][4]['currenttier_patched'];
+        $player5kills = $base['data'][0]['players']['red'][4]['stats']['kills'];
+        $player5deaths = $base['data'][0]['players']['red'][4]['stats']['deaths'];
+        $player5assists = $base['data'][0]['players']['red'][4]['stats']['assists'];
         
         
         
