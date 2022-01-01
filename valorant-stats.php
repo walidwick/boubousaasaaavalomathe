751a934f220b56979c1a5cb0fa733912f36515dc
walidwick <66850558+walidwick@users.noreply.github.com>
2022-01-02 00:53:55 +0100

    Update valorant-stats.php

--- valorant-stats.php
+++ valorant-stats.php
@@ -408,7 +408,7 @@ switch ($request)
         echo "Time Played: ". $timeplayed. " | Matches Played: ". $matchesplayed. " [(". $matcheswon."W-". $matcheslose."L), Win%: ". $win. "] | Total Kills: ". $kills. " (". $HS. " HS) | First Bloods: ". $firstBloods." (". $firstBloodspermatch. " per match) | Clutches: ". $clutches. " |  Aces: ". $aces. " ";
     break;
         //lastmatch test
-    case "teammates":
+    case "redteam":
         $base = _getJSON('https://api.henrikdev.xyz/valorant/v3/by-puuid/matches/eu/fae583c4-c8d4-54ee-ae01-2b0c084865df');
         $gamemode = $base['data'][0]['metadata']['mode'];
         $server = $base['data'][0]['metadata']['cluster'];
