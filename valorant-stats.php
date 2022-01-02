0d5ab4906b2564b061fd2d96ced1765733cf2481
walidwick <66850558+walidwick@users.noreply.github.com>
2022-01-02 20:16:59 +0100

    Update valorant-stats.php

--- valorant-stats.php
+++ valorant-stats.php
@@ -459,8 +459,6 @@ switch ($request)
     break;
     case "blueteam":
         $base = _getJSON('https://api.henrikdev.xyz/valorant/v3/by-puuid/matches/eu/fae583c4-c8d4-54ee-ae01-2b0c084865df');
-        $gamemode = $base['data'][0]['metadata']['mode'];
-        $server = $base['data'][0]['metadata']['cluster'];
         
         $player1name = $base['data'][0]['players']['blue'][0]['name'];
         $player1tag = $base['data'][0]['players']['blue'][0]['tag'];
@@ -506,14 +504,24 @@ switch ($request)
         
         echo "Gamemode: ". $gamemode ." Server: ". $server . ". Blue Team Players: ".$player1name."#".$player1tag.", character: ".$player1character. ", rank: ".$player1rank. ", score: ".$player1kills."/".$player1deaths."/".$player1assists.". | ".$player2name."#".$player2tag.", character: ".$player2character. ", rank: ".$player2rank. ", score: ".$player2kills."/".$player2deaths."/".$player2assists.". | ".$player3name."#".$player3tag.", character: ".$player3character. ", rank: ".$player3rank. ", score: ".$player3kills."/".$player3deaths."/".$player3assists.". | ".$player4name."#".$player4tag.", character: ".$player4character. ", rank: ".$player4rank. ", score: ".$player4kills."/".$player4deaths."/".$player4assists.". | ".$player5name."#".$player5tag.", character: ".$player5character. ", rank: ".$player5rank. ", score: ".$player5kills."/".$player5deaths."/".$player5assists." / / / / weee salina";
     break;
-        //lmsldsmldmdl
+    case "gameinfos":
+        $base = _getJSON('https://api.henrikdev.xyz/valorant/v3/by-puuid/matches/eu/fae583c4-c8d4-54ee-ae01-2b0c084865df');
+        $base2 = _getJSON('https://api.henrikdev.xyz/valorant/v1/mmr/eu/mathematicien/1687');
+
+        $lg = $base2['data']['mmr_change_to_last_game'];
+
+        $map = $base['data'][0]['metadata']['map'];
+        $gamemode = $base['data'][0]['metadata']['mode'];
+        $server = $base['data'][0]['metadata']['cluster'];
+        echo "Gamemode: ".$gamemode." server: ".$server." map: ".$map." MMR changement: ".$lg."";
+    break;
     case "gameresult":
         $base = _getJSON('https://api.henrikdev.xyz/valorant/v3/by-puuid/matches/eu/fae583c4-c8d4-54ee-ae01-2b0c084865df');
         
         $redteamscore = $base['data'][0]['teams']['red']['rounds_won'];
         $blueteamscore = $base['data'][0]['teams']['blue']['rounds_won'];
         
-    echo "Game Score: Blue Team ".$blueteamscore. " | ".$redteamscore. " Red Team";
+        echo "Game Score: Blue Team ".$blueteamscore. " | ".$redteamscore. " Red Team";
     break;
     default:
         echo "fin ghadi a dak rass l 9alwa xDD";
