5404bb73fff1b60095c604c12bdf0aa0ecc925f1
walidwick <66850558+walidwick@users.noreply.github.com>
2022-03-14 00:03:56 +0100

    Update valorant-stats.php

--- valorant-stats.php
+++ valorant-stats.php
@@ -18,20 +18,17 @@ switch ($request)
     // tal gheda
     case "rank":
         //        $base = _getJSON('https://api.tracker.gg/api/v2/valorant/standard/profile/riot/mathematicien%231687');
-        $base = _getJSON('https://api.henrikdev.xyz/valorant/v1/mmr/eu/fezzex/1337');
-        $basel = _getJSON('https://api.tracker.gg/api/v2/valorant/standard/profile/riot/mathematicien%231687');
+        $base = _getJSON('https://api.henrikdev.xyz/valorant/v1/mmr/eu/mathematicien/frax');
 
         $tier = $base['data']['ranking_in_tier'];
         $rank = $base['data']['currenttierpatched'];
-        $top = $basel['data']['segments'][0]['stats']['rank']['rank'];
 
         echo "Mathematicien's current rank is ". $rank." ". $tier."RR (new account)";
     break;
     case "minirank":
-        $base = _getJSON('https://api.henrikdev.xyz/valorant/v1/mmr/eu/fezzex/1337');
-        $base3 = _getJSON('https://api.henrikdev.xyz/valorant/v1/mmr/eu/mathematicien/frax');
+        $base = _getJSON('https://api.henrikdev.xyz/valorant/v1/mmr/eu/mathematicien/frax');
+
         $rank = $base['data']['currenttierpatched'];
-        $srank = $base3['data']['currenttierpatched'];
     echo "Rank: ". $rank. ". Subscribe for details ;)";
     break;
     case "timeplayed":
