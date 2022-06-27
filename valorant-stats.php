bab794bab077da44b916f1a5b7bb3e0af26246f7
walidwick <66850558+walidwick@users.noreply.github.com>
2022-06-27 01:24:41 +0100

    Update valorant-stats.php

--- valorant-stats.php
+++ valorant-stats.php
@@ -19,7 +19,7 @@ switch ($request)
     case "rank":
         //        $base = _getJSON('https://api.tracker.gg/api/v2/valorant/standard/profile/riot/mathematicien%231687');
         $baseeu = _getJSON('https://api.henrikdev.xyz/valorant/v1/mmr/eu/Mathematicien/frax');
-        $basena = _getJSON('https://api.henrikdev.xyz/valorant/v1/mmr/na/mathematicien/las9');
+        $basena = _getJSON('https://api.henrikdev.xyz/valorant/v1/mmr/na/ARR0GANT/TOXIC');
 
         $tiereu = $baseeu['data']['ranking_in_tier'];
         $rankeu = $baseeu['data']['currenttierpatched'];
@@ -31,7 +31,7 @@ switch ($request)
     break;
     case "minirank":
         $baseeu = _getJSON('https://api.henrikdev.xyz/valorant/v1/mmr/eu/Mathematicien/frax');
-        $basena = _getJSON('https://api.henrikdev.xyz/valorant/v1/mmr/na/mathematicien/las9');
+        $basena = _getJSON('https://api.henrikdev.xyz/valorant/v1/mmr/na/ARR0GANT/TOXIC');
         
         $rankeu = $baseeu['data']['currenttierpatched'];
         $rankna = $basena['data']['currenttierpatched'];
@@ -412,7 +412,7 @@ switch ($request)
     break;
         //lastmatch test
     case "redteam":
-        $base = _getJSON('https://api.henrikdev.xyz/valorant/v3/by-puuid/matches/eu/882a4c1c-fc72-521b-904e-8bcec9726d5d');
+        $base = _getJSON('https://api.henrikdev.xyz/valorant/v3/by-puuid/matches/na/a96c09fd-88c6-5e0c-a9e7-019e90c6bd49');
         $gamemode = $base['data'][0]['metadata']['mode'];
         $server = $base['data'][0]['metadata']['cluster'];
         
@@ -461,7 +461,7 @@ switch ($request)
         echo "Red Team Players: ".$player1name."#".$player1tag.", character: ".$player1character. ", rank: ".$player1rank. ", score: ".$player1kills."/".$player1deaths."/".$player1assists." | ".$player2name."#".$player2tag.", character: ".$player2character. ", rank: ".$player2rank. ", score: ".$player2kills."/".$player2deaths."/".$player2assists." | ".$player3name."#".$player3tag.", character: ".$player3character. ", rank: ".$player3rank. ", score: ".$player3kills."/".$player3deaths."/".$player3assists." | ".$player4name."#".$player4tag.", character: ".$player4character. ", rank: ".$player4rank. ", score: ".$player4kills."/".$player4deaths."/".$player4assists." | ".$player5name."#".$player5tag.", character: ".$player5character. ", rank: ".$player5rank. ", score: ".$player5kills."/".$player5deaths."/".$player5assists." ";
     break;
     case "blueteam":
-        $base = _getJSON('https://api.henrikdev.xyz/valorant/v3/by-puuid/matches/eu/882a4c1c-fc72-521b-904e-8bcec9726d5d');
+        $base = _getJSON('https://api.henrikdev.xyz/valorant/v3/by-puuid/matches/na/a96c09fd-88c6-5e0c-a9e7-019e90c6bd49');
         
         $player1name = $base['data'][0]['players']['blue'][0]['name'];
         $player1tag = $base['data'][0]['players']['blue'][0]['tag'];
@@ -508,8 +508,8 @@ switch ($request)
         echo "Blue Team Players: ".$player1name."#".$player1tag.", character: ".$player1character. ", rank: ".$player1rank. ", score: ".$player1kills."/".$player1deaths."/".$player1assists." | ".$player2name."#".$player2tag.", character: ".$player2character. ", rank: ".$player2rank. ", score: ".$player2kills."/".$player2deaths."/".$player2assists." | ".$player3name."#".$player3tag.", character: ".$player3character. ", rank: ".$player3rank. ", score: ".$player3kills."/".$player3deaths."/".$player3assists." | ".$player4name."#".$player4tag.", character: ".$player4character. ", rank: ".$player4rank. ", score: ".$player4kills."/".$player4deaths."/".$player4assists." | ".$player5name."#".$player5tag.", character: ".$player5character. ", rank: ".$player5rank. ", score: ".$player5kills."/".$player5deaths."/".$player5assists." ";
     break;
     case "gameinfos":
-        $base = _getJSON('https://api.henrikdev.xyz/valorant/v3/by-puuid/matches/eu/882a4c1c-fc72-521b-904e-8bcec9726d5d');
-        $base2 = _getJSON('https://api.henrikdev.xyz/valorant/v1/by-puuid/mmr/eu/882a4c1c-fc72-521b-904e-8bcec9726d5d');
+        $base = _getJSON('https://api.henrikdev.xyz/valorant/v3/by-puuid/matches/na/a96c09fd-88c6-5e0c-a9e7-019e90c6bd49');
+        $base2 = _getJSON('https://api.henrikdev.xyz/valorant/v1/by-puuid/mmr/na/a96c09fd-88c6-5e0c-a9e7-019e90c6bd49');
 
         $lg = $base2['data']['mmr_change_to_last_game'];
 
@@ -519,7 +519,7 @@ switch ($request)
         echo "Gamemode: ".$gamemode." | Server: ".$server." | Map: ".$map." | MMR change: ".$lg."";
     break;
     case "gameresult":
-        $base = _getJSON('https://api.henrikdev.xyz/valorant/v3/by-puuid/matches/eu/882a4c1c-fc72-521b-904e-8bcec9726d5d');
+        $base = _getJSON('https://api.henrikdev.xyz/valorant/v3/by-puuid/matches/na/a96c09fd-88c6-5e0c-a9e7-019e90c6bd49');
         
         $redteamscore = $base['data'][0]['teams']['red']['rounds_won'];
         $blueteamscore = $base['data'][0]['teams']['blue']['rounds_won'];
